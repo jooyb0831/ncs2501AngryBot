655041cd80d304d1df0507648a84d576a28fd51b
jooyb0831 <dia231@naver.com>
2025-04-04 15:01:23 +0900

    0404

--- Assets/00.Scripts/PhotonManager.cs
+++ Assets/00.Scripts/PhotonManager.cs
@@ -18,6 +18,13 @@ public class PhotonManager : MonoBehaviourPunCallbacks
     //룸 이름을 입력할 TMP_Inputfield;
     public TMP_InputField roomNameIF;
 
+    //룸 목록에 대한 데이터를 저장하기 위한 딕셔너리 자료형
+    private Dictionary<string, GameObject> rooms = new Dictionary<string, GameObject>();
+    //룸 목록을 표시할 프리팹
+    private GameObject roomItemPrefab;
+    //RoomItem 프리팹이 추가될 ScrollContent
+    public Transform scrollContent;
+
     void Awake()
     {
         //마스터 클라이언트의 씬 자동 동기화 옵션
@@ -30,16 +37,31 @@ public class PhotonManager : MonoBehaviourPunCallbacks
         //포톤 서버와의 데이터의 초당 전송 횟수
         Debug.Log(PhotonNetwork.SendRate);
 
+        //RoomItem 프리팹 로드
+        roomItemPrefab = Resources.Load<GameObject>("RoomItem");
+
         //포톤 서버 접속
-        PhotonNetwork.ConnectUsingSettings();
+        if (PhotonNetwork.IsConnected == false)
+        {
+            PhotonNetwork.ConnectUsingSettings();
+        }
+
+    }
+    void Start()
+    {
+        //저장된 유저명을 로드
+        userId = PlayerPrefs.GetString("USER_ID", $"USER_{Random.Range(1, 21):00}");
+        userIF.text = userId;
+        //접속 유저의 닉네임 등록
+        PhotonNetwork.NickName = userId;
     }
 
     //유저명을 설정하는 로직
     public void SetUserId()
     {
-        if(string.IsNullOrEmpty(userIF.text))
+        if (string.IsNullOrEmpty(userIF.text))
         {
-            userId = $"USER_{Random.Range(1,21):00}";
+            userId = $"USER_{Random.Range(1, 21):00}";
         }
         else
         {
@@ -55,21 +77,12 @@ public class PhotonManager : MonoBehaviourPunCallbacks
     //룸 명의 입력 여부를 확인하는 로직
     string SetRoomName()
     {
-        if(string.IsNullOrEmpty(roomNameIF.text))
+        if (string.IsNullOrEmpty(roomNameIF.text))
         {
-            roomNameIF.text = $"ROOM_{Random.Range(1,101):000}";
+            roomNameIF.text = $"ROOM_{Random.Range(1, 101):000}";
         }
         return roomNameIF.text;
 
-    }
-
-    void Start()
-    {
-        //저장된 유저명을 로드
-        userId = PlayerPrefs.GetString("USER_ID", $"USER_{Random.Range(1,21):00}");
-        userIF.text = userId;
-        //접속 유저의 닉네임 등록
-        PhotonNetwork.NickName = userId;   
     }
 
     //포톤 서버에 접속 후 호출되는 콜백 함수
@@ -77,14 +90,14 @@ public class PhotonManager : MonoBehaviourPunCallbacks
     {
         Debug.Log("Connected to Master!");
         Debug.Log($"PhotonNetwork.InLobby = {PhotonNetwork.InLobby}");
-        //PhotonNetwork.JoinLobby();
+        PhotonNetwork.JoinLobby();
     }
 
     //로비에 접속 후 호출되는 콜백 함수
     public override void OnJoinedLobby()
     {
         Debug.Log($"PhotonNetwork.InLobby = {PhotonNetwork.InLobby}");
-        PhotonNetwork.JoinRandomRoom();
+        //PhotonNetwork.JoinRandomRoom();
     }
 
     //랜덤한 룸 입장이 실패했을 경우 호출되는 롤백 함수
@@ -119,8 +132,8 @@ public class PhotonManager : MonoBehaviourPunCallbacks
     {
         Debug.Log($"PhotonNetwork.InRoom = {PhotonNetwork.InRoom}");
         Debug.Log($"Player Count = {PhotonNetwork.CurrentRoom.PlayerCount}");
-        
-        foreach(var player in PhotonNetwork.CurrentRoom.Players)
+
+        foreach (var player in PhotonNetwork.CurrentRoom.Players)
         {
             Debug.Log($"{player.Value.NickName}, {player.Value.ActorNumber}");
         }
@@ -135,15 +148,60 @@ public class PhotonManager : MonoBehaviourPunCallbacks
         */
         //네트워크 객체를 생성하기 위해서는 반드시 PhotonNetwork.Instantiate를 사용해야함.
         //해당 프리팹은 PhotonView컴포넌트를 가지고 있어야 함.
-        
+
         //마스터 클라이언트인 경우에 룸에 입장한 후 전투 씬을 로딩한다.
-        if(PhotonNetwork.IsMasterClient)
+        if (PhotonNetwork.IsMasterClient)
         {
             PhotonNetwork.LoadLevel("BattleField");
         }
     }
 
-#region UI_BUTTON_EVENT
+    //룸 목록을 수신하는 콜백 함수
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        //삭제된 RoomItem 프리팹을 저장할 임시변수
+        GameObject tempRoom = null;
+
+        foreach (var roomInfo in roomList)
+        {
+            //룸이 삭제된 경우
+            if (roomInfo.RemovedFromList)
+            {
+                //딕셔너리에서 룸 이름으로 검색해 저장된 RoomItem 프리팹을 추출
+                rooms.TryGetValue(roomInfo.Name, out tempRoom);
+
+                //RoomItem 프리팹 삭제
+                Destroy(tempRoom);
+
+                //딕셔너리에서 해당 룸 이름의 데이터를 삭제
+                rooms.Remove(roomInfo.Name);
+            }
+            else //룸 정보가 변경된 경우
+            {
+                //룸 이름이 딕셔너리에 없는 경우 새로 추가
+                if (rooms.ContainsKey(roomInfo.Name) == false)
+                {
+                    //RoomInfo프리팹을 scrollContent 하위에 생성
+                    GameObject roomPrefab = Instantiate(roomItemPrefab, scrollContent);
+
+                    //룸 정보를 표시하기 위해 RoomInfo 정보 전달
+                    roomPrefab.GetComponent<RoomData>().RoomInfo = roomInfo;
+
+                    //딕셔너리 자료형에 데이터 추가
+                    rooms.Add(roomInfo.Name, roomPrefab);
+                }
+                else // 룸 이름이 딕셔너리에 있는 경우에 룸 정보를 갱신
+                {
+                    rooms.TryGetValue(roomInfo.Name, out tempRoom);
+                    tempRoom.GetComponent<RoomData>().RoomInfo = roomInfo;
+                }
+            }
+            Debug.Log($"Room = {roomInfo.Name} ({roomInfo.PlayerCount} / {roomInfo.MaxPlayers})");
+        }
+
+    }
+
+    #region UI_BUTTON_EVENT
     public void OnLoginClick()
     {
         //유저명 저장
@@ -164,6 +222,6 @@ public class PhotonManager : MonoBehaviourPunCallbacks
         //룸생성
         PhotonNetwork.CreateRoom(SetRoomName(), ro);
     }
-#endregion
+    #endregion
 
 }
